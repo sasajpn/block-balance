2990d3f6831cf6fd6aeb5ad1f7136ed91e426895
kazuki <s.kazuki.japan@gmail.com>
2024-11-24 18:32:29 +0900

    add analytics

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { Game } from "./matter.ts";
+import { inject } from "@vercel/analytics";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
@@ -6,4 +7,5 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   </div>
 `;
 
+inject();
 Game();
